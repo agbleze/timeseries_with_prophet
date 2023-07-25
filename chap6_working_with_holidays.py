4a65c6061a05c7ac411d3d9a20b2a2696eefb993
agbleze <lagbleze@st.ug.edu.gh>
2023-07-25 22:59:19 +0200

    Create chap6_working_with_holidays.py

---
+++ chap6_working_with_holidays.py
@@ -0,0 +1,25 @@
+#%%
+import pandas as pd
+import matplotlib.pyplot as plt
+from prophet import Prophet
+
+#%%
+
+df = pd.read_csv("data/divvy_daily.csv")
+
+df = df[['date','rides']]
+
+df['date'] = pd.to_datetime(df['date'])
+
+df.columns = ['ds','y']
+
+#%%
+model = Prophet(seasonality_mode='multiplicative', yearly_seasonality=4)
+
+model.add_country_holidays(country_name='US')
+
+#%%
+model.fit(df)
+
+
+
